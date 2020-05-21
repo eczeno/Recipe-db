f83931e46334b6bb1c8a4aaec74c5af7d7214517
eczeno <e.chancellor.zeno@gmail.com>
2020-05-21 12:52:46 -0700

    use getenv

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from os import environ, path
+from os import getenv, path
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
@@ -7,5 +7,5 @@ load_dotenv(path.join(basedir, '.env'))
 TESTING = False
 DEBUG = False
 FLASK_ENV = 'development'
-SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
-SECRET_KEY = environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
+SECRET_KEY = getenv('SECRET_KEY')
