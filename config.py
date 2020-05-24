2e328ad4211e7a31fa18ec41c11b8f99e7a40554
eczeno <e.chancellor.zeno@gmail.com>
2020-05-24 14:21:29 -0700

    change procfile

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv, path
 from dotenv import load_dotenv
 
-# basedir = path.abspath(path.dirname(__file__))
-# load_dotenv(path.join(basedir, '.env'))
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
 
 TESTING = False
 DEBUG = False
-# FLASK_ENV = 'development'
-# SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
-# SECRET_KEY = getenv('SECRET_KEY')
+FLASK_ENV = 'development'
+SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
+SECRET_KEY = getenv('SECRET_KEY')
